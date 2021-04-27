ac68bc342e03b372413641989d7030711a5b16cf
garjones <gareth@gareth.com>
2021-04-26 19:01:05 -0700

    Update tomcat.sh

--- tomcat.sh
+++ tomcat.sh
@@ -7,6 +7,8 @@
 # // To run this script remotely:
 # //   apt -y install curl
 # //   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/garjones/gareth.com/master/tomcat.sh)"
+# //
+# // Based on - https://www.tecmint.com/install-apache-tomcat-on-debian-10/
 # //----------------------------------------------------------------------------------------------------------
 # // Gareth Jones - gareth@gareth.com
 # //----------------------------------------------------------------------------------------------------------
@@ -33,8 +35,6 @@ function pause() {
 # //----------------------------------------------------------------------------------------------------------
 # // main()
 # //----------------------------------------------------------------------------------------------------------
-# https://www.tecmint.com/install-apache-tomcat-on-debian-10/
-
 # update sources, install required components and upgrade the entire system
 pause 'update and upgrade [Enter]'
 apt update
@@ -64,31 +64,34 @@ chmod -R g+r conf
 chmod g+x conf
 chown -R tomcat webapps/ work/ temp/ logs/
 
-# this command displays the JAVA_HOME
-pause 'display JAVA_HOME [Enter]'
-update-java-alternatives -l
+# create a tomcat systemd service file
+pause 'create a tomcat systemd service file [Enter]'
+wget https://raw.githubusercontent.com/garjones/gareth.com/master/tomcat.service -P /etc/systemd/system/
 
-# creatre a tomcat systemd service file
-# wget /etc/systemd/system/tomcat.service
+# refresh services & start the tomcat service
+pause 'refresh services & start the tomcat service [Enter]'
+systemctl daemon-reload
+systemctl start tomcat
+systemctl enable tomcat
 
-#systemctl daemon-reload
-#systemctl start tomcat
-#systemctl status tomcat
-#systemctl enable tomcat
+# ask for web manager username and password
+read -p "Username:" user_var  
+read -p "Password:" pwd_var  
 
-# sudo nano /opt/tomcat/conf/tomcat-users.xml
-#####
-# <role rolename="admin-gui,manager-gui"/> 
-# <user username="admin" password="password" roles="admin-gui,manager-gui"/>
-#####
+# enable login for tomcat manager and host manager
+pause 'enable login for tomcat manager and host manager [Enter]'
+cp /opt/tomcat/conf/tomcat-users.xml /opt/tomcat/conf/tomcat-users.xml.bak
+sed -i "s/<\/tomcat-users>/<role\ rolename=\"admin-gui,manager-gui\"\/>\n<\/tomcat-users>/g"  /opt/tomcat/conf/tomcat-users.xml
+sed -i "s/<\/tomcat-users>/<user username=\"$user_var\" password=\"$pwd_var\" roles=\"admin-gui,manager-gui\"\/>\n<\/tomcat-users>/g"  /opt/tomcat/conf/tomcat-users.xml
 
-# sudo nano /opt/tomcat/webapps/manager/META-INF/context.xml
-# sudo nano /opt/tomcat/webapps/host-manager/META-INF/context.xml
-### comment out VALVE className
+# enable remote login to tomcat manager and host manager by removing ip restriction
+pause 'enable remote login to tomcat manager and host manager [Enter]'
+sed -i.bak '21,22d' /opt/tomcat/webapps/manager/META-INF/context.xml
+sed -i.bak '21,22d' /opt/tomcat/webapps/host-manager/META-INF/context.xml
+sudo systemctl restart tomcat
 
-# sudo systemctl restart tomcat
-
-# now access web ui
+# now you can access web ui
 # http://server_domain_or_IP:8080
 # http://server_domain_or_IP:8080/manager/html
 # http://server_domain_or_IP:8080/host-manager/html/
+
